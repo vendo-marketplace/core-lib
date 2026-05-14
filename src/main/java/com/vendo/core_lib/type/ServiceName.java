5dd0b19c8e7f98b3d9dab3418371120714e3bef7
HaaaamBurger <gemasclashes@gmail.com>
2026-05-14 12:16:44 +0300

    added more service names

--- src/main/java/com/vendo/core_lib/type/ServiceName.java
+++ src/main/java/com/vendo/core_lib/type/ServiceName.java
@@ -5,7 +5,11 @@ public enum ServiceName {
     USER_SERVICE("user-service"),
     PRODUCT_SERVICE("product-service"),
     AUTH_SERVICE("auth-service"),
-    REGISTRY_SERVICE("registry-service");
+    REGISTRY_SERVICE("registry-service"),
+    NOTIFICATION_SERVICE("notification-service"),
+    AWS_SERVICE("aws-service"),
+    INDEXER_SERVICE("indexer-service"),
+    SEARCH_SERVICE("search-service");
 
     private final String serviceName;
 
