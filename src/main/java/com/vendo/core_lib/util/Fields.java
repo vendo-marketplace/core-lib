373fd103f785d28bb676f31ade4f2f5eeb724f4c
Kostiantyn <sinkkostia@gmail.com>
2026-06-30 17:48:53 +0300

    kan-68: ref

--- src/main/java/com/vendo/core_lib/util/Fields.java
+++ src/main/java/com/vendo/core_lib/util/Fields.java
@@ -4,13 +4,13 @@ public final class Fields {
 
     private Fields() {}
 
-    public static String nameOf(Class<?> clazz, String fieldName) {
+    public static String nameOf(Class<?> clas, String fieldName) {
         try {
-            clazz.getDeclaredField(fieldName);
+            clas.getDeclaredField(fieldName);
             return fieldName;
         } catch (NoSuchFieldException e) {
             throw new IllegalArgumentException(
-                "Field '" + fieldName + "' not found in " + clazz.getName()
+                "Field '" + fieldName + "' not found in " + clas.getName()
             );
         }
     }
