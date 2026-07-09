db7967dc0c96d199690e472bcf0aa3ad71fe65d7
HaaaamBurger <gemasclashes@gmail.com>
2026-07-09 10:15:59 +0300

    string separator

--- src/main/java/com/vendo/core_lib/constants/Separators.java
+++ src/main/java/com/vendo/core_lib/constants/Separators.java
@@ -4,6 +4,8 @@ public final class Separators {
 
     private Separators() {}
 
+    public static final String EMPTY_STRING = "";
+    public static final String BLANK_STRING = "";
     public static final String SLASH_SEPARATOR = "/";
     public static final String DOT_SEPARATOR = ".";
 
